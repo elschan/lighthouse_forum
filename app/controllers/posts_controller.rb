4827f3f15fb8f1fa7d7914b6fb5511c4c30ac2a3
elsieC <elschan90@gmail.com>
2015-06-22 21:46:58 +0000

    step 8

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,22 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(
+      title: params[:post][:title],
+      author: params[:post][:author],
+      text: params[:post][:text]
+      )
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
 
 get 'posts', to: 'posts#index'
 get 'posts/:id', to: 'posts#show'
+get 'posts/:id', to: 'posts#show'
+post 'posts', to: 'posts#create'
+
 
 end
 
